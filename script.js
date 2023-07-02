c13cb7ba1cdeb154ae0415ad94b600be4d936174
Fabio <fabioalbertoaraujo@gmail.com>
2023-07-01 21:45:20 -0300

    Entrega Batalha Naval - Sprint 5

--- script.js
+++ script.js
@@ -10,3 +10,35 @@ const seaBoard = [
 /*
   Desenvolva seu código logo abaixo
 */
+
+function allocateShips(shipPositions, initialBoard) {
+  for (let i = 0; i < shipPositions.length; i++) {
+    const [row, col] = shipPositions[i];
+    initialBoard[row][col] = "S";
+  }
+  return initialBoard;
+}
+
+function checkGuesses(guesses, mountedBoard) {
+  for (const guess of guesses) {
+    const [row, col] = guess;
+    if (row >= 0 && row < mountedBoard.length && col >= 0 && col < mountedBoard[0].length) {
+      if (mountedBoard[row][col] === "S") {
+        mountedBoard[row][col] = "X";
+      }
+    }
+  }
+  return mountedBoard;
+}
+
+function checkWinCondition(guessedBoard) {
+  for (let row = 0; row < guessedBoard.length; row++) {
+    for (let col = 0; col < guessedBoard[0].length; col++) {
+      if (guessedBoard[row][col] === "S") {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
